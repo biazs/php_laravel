3ad481b291da5b524f4d620b685b59240410d42a
Fabiana  Zoroastro de Souza <fabianazs@MacBook-Pro-de-Fabiana.local>
2019-10-13 20:17:30 -0300

    First commit

---
+++ app/Http/Controllers/DespesaController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Despesa;
+use Illuminate\Http\Request;
+
+class DespesaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $despesas = Despesa::all();
+        return view('despesa.index', compact('despesas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('despesa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'descricao' => 'required|mas:255',
+            'data' => 'required',
+            'valor' => 'required',
+            'categoria_id' => 'required'
+        ]);
+        $despesa = Despesa::create($validateData);
+
+        return redirect('/despesa')-> with('success','Despesa criada com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Despesa  $despesa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Despesa $despesa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Despesa  $despesa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $despesa = Despesa::findOrFail($id);
+        return view('despesa.edit', compact('despesa'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Despesa  $despesa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validateData = $request->validate([
+            'descricao' => 'required|mas:255',
+            'data' => 'required',
+            'valor' => 'required',
+            'categoria_id' => 'required'
+        ]);
+
+        Despesa::whereId($id) -> update($validateData);
+
+        return redirect('/despesa') -> with('success','Despesa alterada com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Despesa  $despesa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $despesa = Despesa::findOrFail($id);
+        $despesa -> delete();
+
+        return redirect('/despesa') -> with('success','Despesa removida com sucesso!');
+    }
+}
